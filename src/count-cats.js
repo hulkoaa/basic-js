62ab33c2fe87fdad5ea721ee13ff5d5f07e6b211
Aliaksandr Hulko <hulkoaa@gmail.com>
2020-02-21 12:53:01 +0300

    feat: add solution for cat count

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,3 +1,12 @@
- module.exports = function countCats(/* matrix */) {
-   
- };
+ module.exports = function countCats(arr) {
+   let count=0;
+   for(i=0; i<arr.length; i++){
+        for(j=0; j<arr[i].length; j++){
+            if(arr[i][j] == "^^")
+            count= count + 1;
+        }
+        
+   }
+   return count;
+
+ }
